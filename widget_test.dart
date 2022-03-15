00066daacb725e8e12e6ed758dd6864c72743d9f
rwema3 <rwemadominik3@gmail.com>
2022-03-15 18:55:50 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -21,6 +21,3 @@ void main() {
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
