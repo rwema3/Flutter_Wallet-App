1b2cbfa365d6da9cfcc5c77f315c2e3941ca948d
rwema3 <rwemadominik3@gmail.com>
2022-03-15 18:07:42 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -7,4 +7,4 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_wallet_ui_challenge/sr
+import 'package:flutter_wallet_ui_challenge/s
