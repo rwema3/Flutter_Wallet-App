1889919459c6ac2dea4f263619183303d267691d
rwema3 <rwemadominik3@gmail.com>
2022-03-15 18:57:33 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -17,3 +17,7 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(App());
 
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
