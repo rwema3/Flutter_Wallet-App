00c3b7bb83bf593c7d4ad24ef2ac7645c0308e5e
rwema3 <rwemadominik3@gmail.com>
2022-03-15 18:55:45 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -24,9 +24,3 @@ void main() {
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
