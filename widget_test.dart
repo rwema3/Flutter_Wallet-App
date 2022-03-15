4048b10faf4577be238bf274b3628387ca5f5447
rwema3 <rwemadominik3@gmail.com>
2022-03-15 17:58:24 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -6,4 +6,5 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.da
+import 'package:flutter_test/flutter_test.dart';
+imp
