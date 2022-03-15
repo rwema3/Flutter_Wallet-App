0422a302fa4e47b233da65fd757e0e32b07a8fa0
rwema3 <rwemadominik3@gmail.com>
2022-03-15 18:15:18 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -13,4 +13,4 @@ import 'package:flutter_wallet_ui_challenge/src/app.dart';
 
 
 void main() {
-  testWidgets('Counter increments smoke test', (Widg
+  testWidgets('Counter increments smoke test', (Widge
