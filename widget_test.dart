d5dfc40859085238b1f1f55851f88f1ca500a780
rwema3 <rwemadominik3@gmail.com>
2022-03-16 17:13:23 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -1,4 +1,4 @@
-// This is a basic Flutter widget test.
+
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility that Flutter provides. For example, you can send tap and scroll
@@ -22,7 +22,7 @@ void main() {
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add))
     await tester.pump();
 
     // Verify that our counter has incremented.
