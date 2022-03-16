97e2546bcba36564b39f98988db0e071f1deb3ba
rwema3 <rwemadominik3@gmail.com>
2022-03-16 17:19:37 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -18,8 +18,8 @@ import 'package:flutter_wallet_ui_challenge/src/app.dart';
     await tester.pumpWidget(App());
 
     // Verify that our counter starts at 0.
-
-
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add))
