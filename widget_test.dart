24e9421ec282ec5a2fab8dbb10b45f3896c68bf5
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-17 20:03:42 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -1,6 +1,4 @@
-  
-
-    // Verify that our counter has incremented.
+  // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
