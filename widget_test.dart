fe71a167dc33291af51d9f38188e48aa6949fc7b
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-17 14:59:48 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -1,4 +1,4 @@
-// This is a bas
+// This is a basi
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_wallet_ui_challenge/src/app.dart';
