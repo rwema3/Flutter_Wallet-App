15a72da8940a33fd6879a17742a12b2189da750d
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-17 20:12:38 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_wallet_ui_challenge/src/app.dart';
 
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
