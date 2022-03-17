ca90e484847811754e52f4d9407f660251abffd7
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-17 11:43:02 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -3,7 +3,7 @@
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget prop
+// tree, read text, and verify that the values of widget pro
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_wallet_ui_challenge/src/app.dart';
