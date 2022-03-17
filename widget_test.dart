c03a317c29e388b63ce4b87e3adf77c27ce8f97e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-17 20:00:31 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -1,5 +1,5 @@
 // This is a basic Flutter widget test.
-//
+/
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
