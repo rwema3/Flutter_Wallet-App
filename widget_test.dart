47c1edbe5bf9955eb4e7837c54ac0c41f6cf86d6
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-17 11:58:53 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -1,4 +1,4 @@
-// This is a basic Flutter widge
+// This is a basic Flutter widg
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_wallet_ui_challenge/src/app.dart';
