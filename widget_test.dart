93a595e6338e213da9122e3efb844fdc4419c9bf
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-17 15:00:45 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -1,4 +1,4 @@
-// This is a basic Flutter widget tes
+// This is a basic Flutter widget test
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_wallet_ui_challenge/src/app.dart';
