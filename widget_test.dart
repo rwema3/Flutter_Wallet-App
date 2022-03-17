64b5b1806508ee5cdee9893192cf9f921ece69ab
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-17 11:53:44 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -1,6 +1,6 @@
 // This is a basic Flutter widget test.
 //
-// To perform an interaction with a widget in your test, use the W
+// To perform an interaction with a widget in your test, use the 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_wallet_ui_challenge/src/app.dart';
