652b7ca0c513e06474ba524f74897af481d3b448
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-17 15:16:17 +0200

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -4,6 +4,7 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_wallet_ui_challenge/src/app.dart';
